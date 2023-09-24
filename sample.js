27b09d0222b3f8e43a6c93056848fc631a239039
KyawNaing145 <kyawnaing.option@gmail.com>
2023-09-24 11:56:28 +0630

    Added new sample.js

---
+++ sample.js
@@ -0,0 +1,22 @@
+myObj = [
+    {
+        "id":"1",
+        "FirstName":"Benjamin",
+        "LastName":"Finkel",
+        "Email":"ben.finkel@cbtnetggets.com"
+    },
+    {
+        "id":"2",
+        "FirstName":"Jane",
+        "LastName":"doe",
+        "Email":"jane.doe@cbtnetggets.com"
+    },
+    {
+        "id":"3",
+        "FirstName":"Pat",
+        "LastName":"Smith",
+        "Email":"pat.smith@cbtnetggets.com"
+    }
+]
+
+console.log(myOBJ[0]);
